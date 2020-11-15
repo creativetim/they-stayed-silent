8d280876d01ce33df49229551a0af94a4dc6b065
Tim Yager <tim.yager@gmail.com>
2020-11-15 13:26:23 -0600

    A small amount of polish as well as context.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import CongressJSON from "../../data/congress-116.json";
 
+import "../app.css";
+import githubLogo from "../../public/github.png"
+
 const displayProof = (recognizedBiden = {}) => {
-  const { date = '', proof = '' } = recognizedBiden;
+  const { date = "", proof = "" } = recognizedBiden;
 
   if (!date || !proof) {
     return <span>-</span>;
@@ -21,18 +24,40 @@ const displayProof = (recognizedBiden = {}) => {
   }
 
   if (date && proof && !proof.type) {
-  return <span>{ proof }</span>
+    return <span>{proof}</span>;
   }
 };
 
 export default () => {
   return (
-    <div style={{ textAlign: "center" }}>
+    <main>
       <h1>They Stayed Silent</h1>
+      <p className="intro">
+        &ldquo;...Americans are gonna count something else starting right now.
+        They&rsquo;re gonna count who was willing to speak up against Donald
+        Trump trying to kill democracy. And they&rsquo;ll count who will stay
+        silent in the face of this desparete attack of the bedrock insitution of
+        this truly great nation. &lsquo;Cause he just attacked{" "}
+        <em>the thing</em> that makes us so great and it is time for you all to
+        mean what your hats have been yelling.&rdquo;
+        <br />
+        <br />
+        <small>
+          Excerpt from Stephen Colbert&rsquo;s 11/05/20&nbsp;
+          <a
+            href="https://www.youtube.com/watch?v=TeSiJmLoJd0"
+            rel="noreferrer"
+            target="_blank"
+          >
+            monologue
+          </a> (quote begins at 5:23)
+        </small>
+      </p>
+      <hr />
       <table>
         <thead>
           <tr>
-            <td>
+            <td align="left">
               <strong>Name</strong>
             </td>
             <td>
@@ -49,7 +74,7 @@ export default () => {
 
             return (
               <tr key={id.govtrack}>
-                <td>{name.official_full}</td>
+                <td align="left">{name.official_full}</td>
                 <td>{currentTerm.party}</td>
                 <td>{displayProof(recognizedBiden)}</td>
               </tr>
@@ -57,6 +82,18 @@ export default () => {
           })}
         </tbody>
       </table>
-    </div>
+      <hr />
+      <a
+        href="https://github.com/creativetim/they-stayed-silent"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img
+          src={githubLogo}
+          alt="@creativetim/they-stayed-silent"
+          width="16"
+        />
+      </a>
+    </main>
   );
 };
